98c421a88f1f04440258d2ebfdc75f0e945c3eba
Aleksey <lexa.shvetsoff@yandex.ru>
2022-04-08 13:10:52 +0800

    Рефакторинг кода

--- main.py
+++ main.py
@@ -7,17 +7,11 @@ from pathvalidate import sanitize_filename
 from dotenv import load_dotenv
 from random import randint
 
-load_dotenv()
-
-# URL_COMICS = 'https://xkcd.com/353/info.0.json'
 URL_LAST_COMICS = 'https://xkcd.com/info.0.json'
 
 URL_API_VK_PHOTO_SERVER = 'https://api.vk.com/method/photos.getWallUploadServer'
 URL_API_VK_SAVE_PHOTO = 'https://api.vk.com/method/photos.saveWallPhoto'
 URL_API_VK_PUBLICATION_PHOTO = 'https://api.vk.com/method/wall.post'
-
-access_token = os.getenv('ACCESS_TOKEN')
-group_id = os.getenv('PUBLIC_ID')
 
 
 def load_image(url, filename, folder='images', params={}):
@@ -47,64 +41,89 @@ def get_random_url_comics():
     return f'https://xkcd.com/{num_comics}/info.0.json'
 
 
-response = requests.get(get_random_url_comics())
-response.raise_for_status()
-
-comics = response.json()
-
-url_split = urlsplit(comics['img'])
-file_name = sanitize_filename(url_split.path)
-
-img_name = load_image(comics['img'], file_name)
-
-author_comment = comics['alt']
-
-payload = {
-    'group_id': group_id,
-    'access_token': access_token,
-    'v': 5.131
-}
-
-response = requests.get(URL_API_VK_PHOTO_SERVER, params=payload)
-response.raise_for_status()
-
-photo_server = response.json()['response']
-
-with open(img_name, 'rb') as file:
-    url = photo_server['upload_url']
-    files = {
-        'photo': file
-    }
-
-    response = requests.post(url, files=files)
+def get_comics():
+    response = requests.get(get_random_url_comics())
     response.raise_for_status()
 
-loaded_img = response.json()
+    comics = response.json()
 
-payload = {
-    'group_id': group_id,
-    'server': loaded_img['server'],
-    'photo': loaded_img['photo'],
-    'hash': loaded_img['hash'],
-    'access_token': access_token,
-    'v': 5.131,
-}
+    url_split = urlsplit(comics['img'])
+    file_name = sanitize_filename(url_split.path)
 
-response = requests.post(URL_API_VK_SAVE_PHOTO, params=payload)
-response.raise_for_status()
+    return load_image(comics['img'], file_name), comics['alt']
 
-save_img = response.json()['response'][0]
 
-attachments = 'photo{}_{}'.format(save_img['owner_id'], save_img['id'])
-payload = {
-    'owner_id': -int(group_id),
-    'from_group': 1,
-    'attachments': attachments,
-    'message': author_comment,
-    'access_token': access_token,
-    'v': 5.131
-}
+def get_server(group_id, access_token):
+    payload = {
+        'group_id': group_id,
+        'access_token': access_token,
+        'v': 5.131
+    }
 
-response = requests.get(URL_API_VK_PUBLICATION_PHOTO, params=payload)
-response.raise_for_status()
-print(response.json())
+    response = requests.get(URL_API_VK_PHOTO_SERVER, params=payload)
+    response.raise_for_status()
+
+    return response.json()['response']['upload_url']
+
+
+def upload_img_to_server(img_name, photo_server):
+    with open(img_name, 'rb') as file:
+        url = photo_server
+        files = {
+            'photo': file
+        }
+
+        response = requests.post(url, files=files)
+        response.raise_for_status()
+
+    return response.json()['server'], response.json()['photo'], response.json()['hash']
+
+
+def save_img_to_server(group_id, server, photo, hash, access_token):
+    payload = {
+        'group_id': group_id,
+        'server': server,
+        'photo': photo,
+        'hash': hash,
+        'access_token': access_token,
+        'v': 5.131,
+    }
+
+    response = requests.post(URL_API_VK_SAVE_PHOTO, params=payload)
+    response.raise_for_status()
+
+    return response.json()['response'][0]['owner_id'], response.json()['response'][0]['id']
+
+
+def publication_img(owner_id, photo_id, group_id, author_comment, access_token):
+    attachments = 'photo{}_{}'.format(owner_id, photo_id)
+    payload = {
+        'owner_id': -int(group_id),
+        'from_group': 1,
+        'attachments': attachments,
+        'message': author_comment,
+        'access_token': access_token,
+        'v': 5.131
+    }
+
+    response = requests.get(URL_API_VK_PUBLICATION_PHOTO, params=payload)
+    response.raise_for_status()
+    print(response.json())
+
+
+def main():
+    load_dotenv()
+    
+    access_token = os.getenv('ACCESS_TOKEN')
+    group_id = os.getenv('PUBLIC_ID')
+
+    img_name, author_comment = get_comics()
+    photo_server = get_server(group_id, access_token)
+    server, photo, hash = upload_img_to_server(img_name, photo_server)
+    owner_id, photo_id = save_img_to_server(group_id, server, photo, hash, access_token)
+
+    publication_img(owner_id, photo_id, group_id, author_comment, access_token)
+
+
+if __name__ == '__main__':
+    main()
